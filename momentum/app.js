85fef90e34a6db97262039223e15e5435266e6eb
clappingmin <clappingmin@naver.com>
2022-01-19 16:40:52 +0900

    toggle

--- momentum/app.js
+++ momentum/app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector("div.hello h1");
 
 
 function handleTitleClick() {
-  const clickedClass = "active";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass)
-  }
+  h1.classList.toggle("active");
 }
 
 
